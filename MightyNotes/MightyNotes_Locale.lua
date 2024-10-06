98a4c5ff09a1dc9e8fdd444810e12b99b88a44ab
Sandro <59252676+IAmMrMighty@users.noreply.github.com>
2024-10-06 20:51:53 +0200

    Add files via upload

---
+++ MightyNotes/MightyNotes_Locale.lua
@@ -0,0 +1,21 @@
+---------------------------------
+--     MightyNotes Locale
+---------------------------------
+
+local LANG = GetLocale()
+local void, MightyNotes = ...
+local function localeFunc(L, key) return key end
+local L = setmetatable({}, {__index = localeFunc})
+MightyNotes.L = L
+
+if LANG == "deDE" then
+    L["Save"] = "Speichern";
+    L["Delete"] = "Löschen";
+    L["Place notes here"] = "Hier ist Platz für deine Notizen.";
+    L["New note added!"] = "Neue Notiz hinzugefügt.";
+    L["Note deleted!"] = "Notiz gelöscht.";
+    L['Update'] = "Aktualisieren";
+    L["First note added!"] = "Erste Notiz hinzugefügt.";
+    L["Note "] = "Notiz ";
+    L["Create a new note or open one."] = "Erstelle eine Notiz oder öffne eine.";
+end
